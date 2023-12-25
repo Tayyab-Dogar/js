f058fae9dc183121be3371c413e3a3cab29cfe78
Tayyab-Dogar <154096511+Tayyab-Dogar@users.noreply.github.com>
2023-12-25 19:46:58 +0000

    container config done

--- day-1/variable.js
+++ day-1/variable.js
@@ -2,3 +2,12 @@ let stdName = "tayyab";
 let stdRoll = 1;
 let stdClass = "MS DS";
 console.table([stdRoll,stdName,stdClass]);
+/* 
+
+we can create variable by 3 keyword var, let and const.
+if we create variable by using const then we cant change its value.
+if we create variable using let than its has block scope 
+if we create variable using var than it has globle scope (we can get/change it anywhere in the program)
+note 
+dont use var varibles keyword because of block and function scope issue.
+*/
